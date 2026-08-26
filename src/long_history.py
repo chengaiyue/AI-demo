55f0b84846b11ec3ac29ed4ba77ccdc4930831ea
zcc <zcc@zccdeMacBook-Pro.local>
2026-08-26 21:47:34 +0800

    优化历史记录

--- src/long_history.py
+++ src/long_history.py
@@ -77,5 +77,7 @@ _conversation_chain = RunnableWithMessageHistory(
 session_config = {"configurable": {"session_id": "user_001"}}
 
 res = _conversation_chain.invoke({"input": "小明有一只猫"}, session_config)
-res2 = conversation_chain.invoke({"input": "小明有一条狗"}, session_config)
-# res3 = conversation_chain.invoke({"input": "小明有几个宠物"}, session_config)
+res2 = _conversation_chain.invoke({"input": "小明有一条狗"}, session_config)
+res3 = _conversation_chain.invoke({"input": "小明有几个宠物"}, session_config)
+
+print(res3);
