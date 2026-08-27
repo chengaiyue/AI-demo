8f5f303b3d634a2357f25893fdcb449aa0c6e45a
zhangchengcheng <zhangchengcheng@beisen.com>
2026-08-27 18:56:00 +0800

    向量

---
+++ src/embeddings.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import OpenAIEmbeddings
+
+from langchain_core.document_loaders import CSVLoader
+
+from langchain_chroma import Chroma
+
+openai_embeddings = OpenAIEmbeddings(
+    model="doubao-embedding-vision",
+    chunk_size=100,
+    chunk_overlap=0,
+    api_key=os.environ.get("EMBEDDING_MODEL_API_KEY"),
+    base_url="https://ark.cn-beijing.volces.com/api/plan/v3",
+)
+
+vectorstore = InMemoryVectorStore(  # 内存向量存储
+    embeddings=openai_embeddings,
+)
+
+loader = CSVLoader(
+    file_path="./file_collection/demo.csv",
+    encoding="utf-8",
+)
+
+loader.load()
+
+
