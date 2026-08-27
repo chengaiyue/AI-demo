67132e6ec186aad35eb599ae28ed48f07f875628
zcc <zcc@zccdeMacBook-Pro.local>
2026-08-27 23:09:51 +0800

    查询

--- src/embeddings.py
+++ src/embeddings.py
@@ -5,20 +5,18 @@ load_dotenv()
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_openai import OpenAIEmbeddings
 
-from langchain_core.document_loaders import CSVLoader
-
-from langchain_chroma import Chroma
+from langchain_community.document_loaders import CSVLoader
 
 openai_embeddings = OpenAIEmbeddings(
     model="doubao-embedding-vision",
     chunk_size=100,
-    chunk_overlap=0,
+    check_embedding_ctx_length=False, # 需要加这个参数，不然报错
     api_key=os.environ.get("EMBEDDING_MODEL_API_KEY"),
     base_url="https://ark.cn-beijing.volces.com/api/plan/v3",
 )
 
 vectorstore = InMemoryVectorStore(  # 内存向量存储
-    embeddings=openai_embeddings,
+    embedding=openai_embeddings,
 )
 
 loader = CSVLoader(
@@ -33,3 +31,9 @@ vectorstore.add_documents(
     ids=[str(f"id{i}") for i in range(len(documents))]
 )
 
+res = vectorstore.similarity_search(
+    query="123456789",
+    k=1,
+)
+
+print(res)
