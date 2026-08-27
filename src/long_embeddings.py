dd709f89dce7db2bc82b132be7b01066bf301c6d
zcc <zcc@zccdeMacBook-Pro.local>
2026-08-28 00:09:18 +0800

    向量数据库

---
+++ src/long_embeddings.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import OpenAIEmbeddings
+
+from langchain_community.document_loaders import CSVLoader
+
+from langchain_chroma import Chroma
+
+openai_embeddings = OpenAIEmbeddings(
+    model="doubao-embedding-vision",
+    chunk_size=100,
+    check_embedding_ctx_length=False, # 需要加这个参数，不然报错
+    api_key=os.environ.get("EMBEDDING_MODEL_API_KEY"),
+    base_url="https://ark.cn-beijing.volces.com/api/plan/v3",
+)
+
+vectorstore = Chroma(  # 内存向量存储
+    embedding=openai_embeddings,
+)
+
+loader = CSVLoader(
+    file_path="./file_collection/demo.csv",
+    encoding="utf-8",
+    source_column="name",
+)
+
+documents = loader.load()
+
+vectorstore.add_documents(
+    documents=documents,
+    ids=[str(f"id{i}") for i in range(len(documents))]
+)
+
+res = vectorstore.similarity_search(
+    query="123456789",
+    k=1,
+)
+
+print(res)
